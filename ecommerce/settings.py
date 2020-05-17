ff0cc4c3fe096ab15ba72bdb800e19d0c304b8af
comanezz <aymeric.lcharles@gmail.com>
2020-05-18 00:09:25 +0100

    Heroku host added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), 'aymeric-ecommerce.herokuapp.com']
 
 
 # Application definition
